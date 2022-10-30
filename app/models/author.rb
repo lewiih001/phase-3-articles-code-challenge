3bd9665af9885761fe4a15f69f270c768b7d8526
lewiih001 <lewismunyambu@gmail.com>
2022-10-30 22:36:33 +0300

    work in progress

--- app/models/author.rb
+++ app/models/author.rb
@@ -10,12 +10,15 @@ class Author
   def articles
     Article.all.filter {|article| article.author == self}
   end
+
   def magazines
     self.articles.map {|article| article.magazine}.uniq
   end
+
   def add_article(magazine, title)
     Article.new(self, magazine, title)
   end
+
   def topic_areas
     self.magazines.map {|magazine|magazine.category}.uniq
 end
